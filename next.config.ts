a2067ff2219528cfe12da659056e7bf8a5e74a8b
Daniel Mwamba <danmuamba81@gmail.com>
2025-11-03 15:58:15 +0100

    Add Prisma dependency and update configuration

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
       "127.0.0.1",
       "lively-horse-7cb2f4c550.strapiapp.com",
       "lively-horse-7cb2f4c550.media.strapiapp.com",
+      "informed-activity-db79c3338c.media.strapiapp.com",
+      "informed-activity-db79c3338c.strapiapp.com",
     ],
   },
 };
