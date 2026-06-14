702d294427a81cf3bd8e84c4e640c9b1803c6ad4
gadkabula <gadkabula002@gmail.com>
2026-06-14 18:16:01 +0100

    space

--- src/app/don-merci/page.tsx
+++ src/app/don-merci/page.tsx
@@ -4,9 +4,11 @@ import DonationVerifier from "./DonationVerifier";
 export default function DonationThankYouPage() {
   return (
     <MainLayout>
+
       <main className="min-h-[calc(100vh-96px)] bg-[#f4efe4] px-4 py-10 text-primary sm:px-6 sm:py-16 lg:px-8 lg:py-20">
         <DonationVerifier />
       </main>
+      
     </MainLayout>
   );
 }
